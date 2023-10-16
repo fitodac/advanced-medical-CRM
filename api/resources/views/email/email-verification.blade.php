7d0358f628bd9d4e75c92f2eb38b765e8cf43276
fitodac <fitodac@gmail.com>
2023-10-16 16:04:12 -0300

    Fix de guardado de usuarios

--- api/resources/views/email/email-verification.blade.php
+++ api/resources/views/email/email-verification.blade.php
@@ -1,2 +1,40 @@
-{{ __('Please click the button below to verify your email address.') }}
-<a href="{{ route('user.verify', $token) }}">{{ __('Verify Email Address') }}</a>
+<style>
+	body{
+		background: #EEE;
+		padding: 0;
+		margin: 0;
+	}
+</style>
+
+<table 
+	align="center" 
+	style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;">
+		<tr>
+			<td style="height:20px;"></td>
+		</tr>
+		<tr>
+			<td>
+
+				<table style="background-color: white; padding: 30px;">
+					<tr>
+						<td>
+							<p>{{ __('Please click the button below to verify your email address.') }}</p>
+						</td>
+					</tr>
+
+					<tr>
+						<td style="text-align: center;">
+							<br>
+							<br>
+							<a 
+								href="{{ route('user.verify', $token) }}"
+								style="background: black; color: white; padding: 10px 30px; border-radius: 8px; display: inline-block; text-decoration: none;">
+								{{ __('Verify Email Address') }}
+							</a>
+						</td>
+					</tr>
+				</table>
+
+			</td>
+		</tr>
+	</table>
